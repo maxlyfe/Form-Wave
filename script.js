fd71edda53780fa79a5a8578f9d7a82b801243b4
MaxLyFe <maximilianolyfbuzios@gmail.com>
2023-02-24 19:57:16 -0300

    js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+/* Seleccionando todas las etiquetas en la clase de control de formulario. */
 const labels = document.querySelectorAll('.form-control label');
 
 labels.forEach(label => {
+    /* Establecer el HTML interno de la etiqueta en el texto interno de la etiqueta. */
     label.innerHTML = label.innerText
-    .split('')
-    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${latter}</span`)
-    .join('')
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${latter}</span`)
+        .join('')
 })
